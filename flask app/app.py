0a19a119fa71c130434eb75499147e4c64670c54
Karan Singh Negi <knegi3422@gmail.com>
2024-07-03 18:44:49 +0530

    Update app.py

--- flask app/app.py
+++ flask app/app.py
@@ -29,4 +29,4 @@ def delete_user(user_id):
     return jsonify({'error': 'User not found'}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8181)
